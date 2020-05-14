a5af9a12e18ce0a61a1fe5989bd2c24a19b5f117
stefanomarrone <stefano.marrone@unicampania.it>
2020-05-14 12:27:55 +0200

    By adding the classes contained in performing, one can be able to add a behaviour to a component running as a working process doing something...

---
+++ core/performing.py
@@ -0,0 +1,43 @@
+from components import Component
+from blackboard import Blackboard
+from simpy import Interrupt
+from log import Loggable
+
+
+class SimpleBehaviour(Loggable):
+    def __init__(self, nname, qqname, eenv, dettime):
+        super().__init__(nname, qqname, eenv)
+        self.deltatime = dettime
+        self.onrun = True
+        self.infinite = Blackboard().get('stoptime')
+        self.process = self.env.process(self.run())
+
+    def do(self):
+        yield self.env.timeout(self.deltatime)
+        if (self.onrun == True):
+            self.log('is working;;')
+
+    def run(self):
+        while True:
+            try:
+                if (self.onrun == True):
+                    yield self.env.process(self.do())
+                else:
+                    yield self.env.timeout(self.infinite)
+            except Interrupt as i:
+                self.onrun = not self.onrun
+
+
+class Performing(Component):
+
+    def __init__(self, nname, qqueue, eenv, bbehaviour, mmtbf=0, mmttr=0):
+        super().__init__(nname, qqueue, eenv, mmtbf, mmttr)
+        self.behaviour = bbehaviour
+
+    def faultPropagation(self,cause=''):
+        super().faultPropagation(cause)
+        self.behaviour.process.interrupt()
+
+    def repairPropagation(self):
+        super().repairPropagation()
+        self.behaviour.process.interrupt()
