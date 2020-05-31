0714633fa3b42cc5261418a681ef960e3b3bc47f
Carlo Abate <carlo.bta@gmail.com>
2020-05-31 19:24:32 +0200

    implemented core_06 - repairable onboard

--- master/test.py
+++ master/test.py
@@ -154,18 +154,18 @@ def core_06():
     piesse = AndGate('PS',0 ,0)
     piesse.setOwner(global_top)
 
-    piesse1 = Component('PS1',0 ,10)
-    piesse2 = Component('PS2',10 ,10)
-    piesse3 = Component('PS3',0 ,10)
+    piesse1 = Component('PS1',3300000,10)
+    piesse2 = Component('PS2',3300000,10)
+    piesse3 = Component('PS3',3300000,10)
     piesse1.setOwner(piesse)
     piesse2.setOwner(piesse)
     piesse3.setOwner(piesse)
     
-    bus = AndGate('BUS',10 ,0)
+    bus = AndGate('BUS',0 ,0)
     bus.setOwner(global_top)
 
-    bus1 = Component('BUS1',0,15)
-    bus2 = Component('BUS2',0,15)
+    bus1 = Component('BUS1',13500000,15)
+    bus2 = Component('BUS2',13500000,15)
     bus1.setOwner(bus)
     bus2.setOwner(bus)
 
@@ -175,80 +175,53 @@ def core_06():
     voter = AndGate('Voter',10,0)
     voter.setOwner(tmr)
 
-    voter1 = Component('Voter1',0,15)
-    voter2 = Component('Voter2',0,15)
+    voter1 = Component('Voter1',20000000000,15)
+    voter2 = Component('Voter2',20000000000,15)
     voter1.setOwner(voter)
     voter2.setOwner(voter)
 
     cpu = AndGate('CPU',0 , 0)
     cpu.setOwner(tmr)
 
-    cpu1 = Component('CPU1',0, 10)
-    cpu2 = Component('CPU2',0 ,10)
-    cpu3 = Component('CPU3',0 ,10)
+    cpu1 = Component('CPU1',8100000, 10)
+    cpu2 = Component('CPU2',8100000 ,10)
+    cpu3 = Component('CPU3',8100000 ,10)
     cpu1.setOwner(cpu)
     cpu2.setOwner(cpu)
     cpu3.setOwner(cpu)
 
-    btm = AndGate('BTM',0 ,0 )
-    btm.setOwner(global_top)
+    wan = AndGate('WAN',0 ,0 )
+    wan.setOwner(global_top)
 
-    btm1 = Component('BTM1', 0,10 )
-    btm2 = Component('BTM2',10 ,10 )
-    btm1.setOwner(btm)
-    btm2.setOwner(btm)
+    wan1 = Component('WAN1',24000000,10 )
+    wan2 = Component('WAN2',24000000,10 )
+    wan1.setOwner(wan)
+    wan2.setOwner(wan)
 
-    mmi = AndGate('MMI',10 ,0 )
-    mmi.setOwner(global_top)
+    gsm = AndGate('GSM-R',0 ,0 )
+    gsm.setOwner(global_top)
 
-    mmi1 = Component('MMI1', 10,10 )
-    mmi2 = Component('MMI2',10 ,10 )
-    mmi1.setOwner(mmi)
-    mmi2.setOwner(mmi)
+    gsm1 = Component('GSM-R1',10500000,10 )
+    gsm2 = Component('GSM-R2',10500000,10 )
+    gsm1.setOwner(gsm)
+    gsm2.setOwner(gsm)
 
-    tiu = AndGate('TIU',0 ,0 )
-    tiu.setOwner(global_top)
-
-    tiu1 = Component('TIU1',10 ,10 )
-    tiu2 = Component('TIU2',10 ,10 )
-    tiu1.setOwner(tiu)
-    tiu2.setOwner(tiu)
-
-    odo = AndGate('ODO',10 ,10 )
-    odo.setOwner(global_top)
-
-    odo1 = Component('ODO1',0 ,10 )
-    odo2 = Component('ODO2', 0,10)
-    odo1.setOwner(odo)
-    odo2.setOwner(odo)
-
-    rtm = AndGate('RTM', 0, 0)
-    rtm.setOwner(global_top)
-
-    rtm1 = Component('RTM1', 10,10 )
-    rtm2 = Component('RTM2', 0,10)
-    rtm1.setOwner(rtm)
-    rtm2.setOwner(rtm)
-
-    global_top.setSubcomponents([piesse,bus,tmr,btm,mmi,tiu,odo,rtm])
+    global_top.setSubcomponents([piesse,bus,tmr,wan,gsm])
     piesse.setSubcomponents([piesse1,piesse2,piesse3])
     bus.setSubcomponents([bus1,bus2])
     tmr.setSubcomponents([voter,cpu])
     voter.setSubcomponents([voter1,voter2])
     cpu.setSubcomponents([cpu1,cpu2,cpu3])
-    btm.setSubcomponents([btm1,btm2])
-    mmi.setSubcomponents([mmi1,mmi2])
-    tiu.setSubcomponents([tiu1,tiu2])
-    odo.setSubcomponents([odo1,odo2])
-    rtm.setSubcomponents([rtm1,rtm2])
-
+    wan.setSubcomponents([wan1,wan2])
+    gsm.setSubcomponents([gsm1,gsm2])
+    
 fdict = {
     'simple': core_01,
     'structured': core_02,
     'gate_fault': core_03,
     'performing': core_04,
     'on_board': core_05,
-    'onb_param': core_06
+    'onb_repair': core_06
 }
 
 
