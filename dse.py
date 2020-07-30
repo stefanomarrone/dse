0764a077e717ebc6509e371cc7bcec26369893ad
Stefano Marrone <stefano.marrone@unicampania.it>
2020-07-30 12:52:32 +0200

    The batch mode has been added

--- dse.py
+++ dse.py
@@ -1,8 +1,17 @@
 import sys
 from ertms.ertms_sim import ERTMSSimulation, ERTMSConfigurationFactory
+import subprocess
+import multiprocessing
+import sys
+import os
+
+
+def core(inifile, maintfile, logfile, indicesfile):
+    list_files = subprocess.call(["python3", "dse.py", inifile, maintfile, logfile, indicesfile])
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 4:
+    if len(sys.argv) == 5:
         config = sys.argv[1]
         maint = sys.argv[2]
         log = sys.argv[3]
@@ -10,6 +19,21 @@ if __name__ == "__main__":
         confFactory = ERTMSConfigurationFactory()
         simulator = ERTMSSimulation([config, maint], log, indices, confFactory)
         simulator.run()
+    elif len(sys.argv) == 2:
+        directory = sys.argv[1]
+        filenames = list(filter(lambda x: x.endswith('.ini'), os.listdir(directory)))
+        filenames = list(map(lambda x: x[:-4],filenames))
+        jobs = list()
+        for fn in filenames:
+            cn = directory + fn + '.ini'
+            mn = directory + fn + '.mnt'
+            ln = directory + fn + '.log'
+            xn = directory + fn + '.ind'
+            p = multiprocessing.Process(target=core, args=(cn, mn, ln, xn))
+            jobs.append(p)
+            p.start()
     else:
         usage = sys.argv[0] + ' configuration maintenance logs indices'
+        usage += 'or'
+        usage += sys.argv[0] + ' directory'
         print(usage)
